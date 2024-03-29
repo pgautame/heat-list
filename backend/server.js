ef8dedf14a988ec124f1b289f828e7f4268f1d5c
pgautame <59304773+pgautame@users.noreply.github.com>
2024-03-29 21:26:08 +0530

    added API routes

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,23 @@
 require('dotenv').config()
 
 const express = require('express')
+const workoutRoutes = require('./routes/workouts')
 
 // express app
 const app = express()
 
 // middleware
+app.use(express.json())
 app.use((req, res, next) => {
     console.log(req.path, req.method)
     next()
 })
 
 // routes
-app.get('/', (req, res) => { // request, response
-    res.json({ mssg: 'Welcome to the app' })
-})
+app.use('/api/workouts', workoutRoutes)
+// app.get('/', (req, res) => { // request, response
+//     res.json({ mssg: 'Welcome to the app' })
+// })
 
 // listen for requests
 app.listen(process.env.PORT, () => {
